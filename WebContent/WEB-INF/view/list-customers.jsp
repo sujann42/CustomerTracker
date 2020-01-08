25534ff0ced6190cd13d312d9395cc033e97db53
suzan <sujann42@gmail.com>
2020-01-08 12:58:00 -0600

    customer tracker project created.

---
+++ WebContent/WEB-INF/view/list-customers.jsp
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>List Customers</title>
+</head>
+<body>
+List Customers - Coming Soon........
+</body>
+</html>
---
+++ src/com/customer/testdb/TestDbServlet.java
@@ -0,0 +1,40 @@
+package com.customer.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+/**
+ * Servlet implementation class TestDbServlet
+ */
+@WebServlet("/TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String user = "springstudent";
+		String password = "springstudent";
+		String url = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
+		String driver = "com.mysql.cj.jdbc.Driver";
+
+		try {
+			PrintWriter out = response.getWriter();
+			out.println("Connecting to database!!!");
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection(url, user, password);
+			out.println("Connection established!!!");
+			conn.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
